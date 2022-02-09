35c6c67f4e3b74429554d977cd8e0a5bc5e52fe5
Salavat <salavat-kadyrov@bk.ru>
2022-02-09 11:10:26 +0300

    initial commit

---
+++ app/src/main/java/com/example/composite/presentatiion/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.composite.presentatiion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.composite.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
